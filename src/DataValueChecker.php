269dc75afddda090a6d8e8590aedcf4a61d2c2a1
Bene <benestar.wikimedia@gmail.com>
2014-09-20 23:12:05 +0200

    Create DataValueChecker interface

---
+++ src/DataValueChecker.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Wikibase\Constraints;
+
+use Comparable;
+use DataValues\DataValue;
+
+/**
+ * Description of DataValueChecker
+ *
+ * @since 0.1
+ *
+ * @license GNU GPL v2+
+ * @author Bene* < benestar.wikimedia@gmail.com >
+ */
+interface DataValueChecker extends Comparable {
+
+	/**
+	 * Returns if this constraint supports the given data value.
+	 *
+	 * @param DataValue $dataValue
+	 * @return boolean
+	 */
+	public function supportsDataValue( DataValue $dataValue );
+
+	/**
+	 * Returns if the data value passes this constraint.
+	 *
+	 * @param DataValue $dataValue
+	 * @return boolean
+	 */
+	public function checkDataValue( DataValue $dataValue );
+
+	/**
+	 * Returns the unique name of this DataValueChecker.
+	 *
+	 * @return string
+	 */
+	public function getName();
+
+}
